dbd5b19024e3b1a17d87240752dee2c98768b111
Francisco Cardoso <50237391+fdcos-iscteiul@users.noreply.github.com>
2019-10-14 23:24:58 +0100

    Scene transition done
     Transição de cenas feita, atenção, não desligar o LevelChanger

---
+++ Assets/Scripts/LevelChangerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangerScript : MonoBehaviour
+{
+   
+    public Animator animator;
+    private GameObject player;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Player");
+
+        if(Input.GetKeyDown("e")){
+            if(player.transform.position.x >=-8 && player.transform.position.x <= -7){
+                Debug.Log("Enter house");
+                FadeToLevel(1);
+            }
+            if (player.transform.position.x >= 1.1  && player.transform.position.x <= 3)
+            {
+                Debug.Log("Enter Casino");
+                FadeToLevel(2);
+            }
+            if (player.transform.position.x >= 6.1 && player.transform.position.x <= 7)
+            {
+                Debug.Log("Enter arcade");
+                FadeToLevel(3);
+            }
+     }   
+    }
+
+    public void FadeToLevel(int levelIndex){
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete(){
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
