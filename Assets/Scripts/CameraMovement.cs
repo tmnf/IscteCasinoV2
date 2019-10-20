dfe77358a361fce8f082ac6daa581f6723f72ce9
Francisco Cardoso <50237391+fdcos-iscteiul@users.noreply.github.com>
2019-10-20 11:37:42 +0100

    Camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject cam;
+    public float camSpeed;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement = new Vector3 (camSpeed,0,0);
+        cam.transform.position += movement;
+    }
+}
