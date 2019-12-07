7e7a2e107132f700c477b19a90dbd50ffe819133
Francisco Cardoso <50237391+fdcos-iscteiul@users.noreply.github.com>
2019-12-07 22:17:03 +0000

    SlotMachine
    Slot Machine done
    Precisa depois mudar para o que dissemos ao stor, mas já funciona

---
+++ Assets/SlotMachine/GameControl.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour{
+    
+    public static event Action HandlePulled = delegate { };
+ 
+    public Text prizeText;
+ 
+    public Row[] rows;
+
+    public Transform handle;
+
+    private int prizeValue;
+
+    private bool resultsChecked = false;
+
+
+    void Update(){
+        if(!rows[0].rowStopped || !rows[1].rowStopped || !rows[2].rowStopped){
+            prizeValue = 0;
+            prizeText.enabled = false;
+            resultsChecked = false;
+        }
+
+        if(rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped && !resultsChecked){
+           CheckResults();
+           prizeText.enabled = true;
+           prizeText.text = "Prize: " + prizeValue;
+        }
+    }
+
+
+    private void OnMouseDown(){
+        if(rows[0].rowStopped || rows[1].rowStopped || rows[2].rowStopped){
+        StartCoroutine("PullHandle");
+        }
+
+    }
+
+    private IEnumerator PullHandle(){
+        for(int i = 0; i<15; i+=5){
+            handle.Rotate(0f,0f,i);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        HandlePulled();
+
+        for(int i = 0; i < 15; i+=5){
+            handle.Rotate(0f,0f,-i);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void CheckResults(){
+
+        if(rows[0].stoppedSlot == "Diamond" && rows[1].stoppedSlot == "Diamond" && rows[2].stoppedSlot == "Diamond"){
+            prizeValue = 200;
+        }
+
+        else if (rows[0].stoppedSlot == "Crown" && rows[1].stoppedSlot == "Crown" && rows[2].stoppedSlot == "Crown"){
+            prizeValue = 400;
+        }
+
+        else if (rows[0].stoppedSlot == "Melon" && rows[1].stoppedSlot == "Melon" && rows[2].stoppedSlot == "Melon"){
+            prizeValue = 600;
+        }
+        
+        else if (rows[0].stoppedSlot == "Bar" && rows[1].stoppedSlot == "Bar" && rows[2].stoppedSlot == "Bar"){
+            prizeValue = 800;
+        }
+
+        else if (rows[0].stoppedSlot == "Seven" && rows[1].stoppedSlot == "Seven" && rows[2].stoppedSlot == "Seven"){
+            prizeValue = 1500;
+        }
+
+        else if (rows[0].stoppedSlot == "Cherry" && rows[1].stoppedSlot == "Cherry" && rows[2].stoppedSlot == "Cherry"){
+            prizeValue = 3000;
+        }
+
+        else if (rows[0].stoppedSlot == "Lemon" && rows[1].stoppedSlot == "Lemon" && rows[2].stoppedSlot == "Lemon"){
+            prizeValue = 5000;
+        }
+
+        resultsChecked = true;
+
+    }
+}
---
+++ Assets/SlotMachine/Row.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row : MonoBehaviour{
+
+    private int randomValue;
+    private float timeInterval;
+    public bool rowStopped;
+    public string stoppedSlot;
+
+    void Start(){
+        rowStopped = true;
+        GameControl.HandlePulled += StartRotating;
+    }
+
+    private void StartRotating(){
+        stoppedSlot = "";
+        StartCoroutine("Rotate");
+    }
+
+    private IEnumerator Rotate(){
+        rowStopped = false;
+        timeInterval = 0.025f;
+        
+        for(int i = 0; i < 30; i++){
+            if(transform.position.y <= -1.75f){
+                transform.position = new Vector2(transform.position.x, 3.5f);
+            }
+
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
+
+            yield return new WaitForSeconds(timeInterval);
+
+        }
+
+        randomValue = Random.Range(60,100);
+
+        switch(randomValue % 3){
+            case 1:
+                randomValue +=2;
+                break;
+            case 2:
+                randomValue +=1;
+                break;
+        }
+
+        for(int i = 0; i < randomValue; i++){
+            if(transform.position.y <=-1.75){
+                transform.position = new Vector2(transform.position.x, 3.5f);
+            }
+
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
+
+            if(i > Mathf.RoundToInt(randomValue * 0.25f)){
+                timeInterval = 0.05f;
+            }
+
+            if (i > Mathf.RoundToInt(randomValue * 0.5f)){
+                timeInterval = 0.1f;
+            }
+
+            if (i > Mathf.RoundToInt(randomValue * 0.75f)){
+                timeInterval = 0.15f;
+            }
+
+            if (i > Mathf.RoundToInt(randomValue * 0.95f)){
+                timeInterval = 0.2f;
+            }
+
+            yield return new WaitForSeconds(timeInterval);
+
+        }
+
+        if(transform.position.y == -1.75f){
+            stoppedSlot = "Diamond";
+        }
+        else if(transform.position.y == -1f){
+            stoppedSlot = "Crown";
+        }
+        else if (transform.position.y == -0.25f){
+            stoppedSlot = "Melon";
+        }
+        else if (transform.position.y == 0.5){
+            stoppedSlot = "Bar";
+        }
+        else if (transform.position.y == 1.25){
+            stoppedSlot = "Seven";
+        }
+        else if (transform.position.y == 2){
+            stoppedSlot = "Cherry";
+        }
+        else if (transform.position.y == 2.75){
+            stoppedSlot = "Lemon";
+        }
+        else if (transform.position.y == 3.5){
+            stoppedSlot = "Diamond";
+        }
+
+        rowStopped = true;
+    }
+
+    private void onDestroy(){
+        GameControl.HandlePulled -= StartRotating;
+    }
+}
