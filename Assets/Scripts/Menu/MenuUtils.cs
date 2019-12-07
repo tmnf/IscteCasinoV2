04969236e08923e9a6c4a78671f2445abb142124
Tiago Farinha <tiagomnf1999@gmail.com>
2019-12-07 20:22:45 +0000

    Menu Added

---
+++ Assets/Scripts/Menu/MenuUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUtils : MonoBehaviour
+{
+    
+    public void NewGame()
+    {
+
+    }
+    public void LoadGame()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
